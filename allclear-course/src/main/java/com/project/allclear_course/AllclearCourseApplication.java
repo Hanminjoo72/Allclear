5a5b849376a96459dd70895660e7bdf544eadbe3
yeon2lee <md04254@naver.com>
2024-06-28 17:45:28 +0900

    first commit

---
+++ allclear-course/src/main/java/com/project/allclear_course/AllclearCourseApplication.java
@@ -0,0 +1,13 @@
+package com.project.allclear_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllclearCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllclearCourseApplication.class, args);
+	}
+
+}
---
+++ allclear-user/src/main/java/com/project/allclear_user/AllclearUserApplication.java
@@ -0,0 +1,13 @@
+package com.project.allclear_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllclearUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllclearUserApplication.class, args);
+	}
+
+}
