08fd18963fcd67f0c526bebbc17542c452de64fb
mashin2002 <mashin0128@gmail.com>
2024-07-03 00:06:08 +0900

    [feat] login ui

--- allclear-user/src/main/java/com/project/allclear_user/controller/MemberController.java
+++ allclear-user/src/main/java/com/project/allclear_user/controller/MemberController.java
@@ -4,10 +4,8 @@ import com.project.allclear_user.domain.dto.LoginDto;
 import com.project.allclear_user.domain.dto.SignupDto;
 import com.project.allclear_user.service.MemberService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/member")
@@ -29,6 +27,11 @@ public class MemberController {
     public boolean changeInfo(@RequestBody SignupDto.RequestDto requestDto){
        memberService.changeInfo(requestDto);
 
+        return true;
+    }
+
+    @GetMapping("test")
+    public boolean test(){
         return true;
     }
 }
