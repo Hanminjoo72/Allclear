fc7f0b905746fa8562d5d3726c7f2627b9596292
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-08-26 18:21:50 +0900

    [refactor] fix DepartmentRepository #5 lectureExists and update query #17

--- allclear-course/src/main/java/com/project/allclear_course/repository/DepartmentRepository.java
+++ allclear-course/src/main/java/com/project/allclear_course/repository/DepartmentRepository.java
@@ -3,5 +3,8 @@ package com.project.allclear_course.repository;
 import com.project.allclear_course.domain.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    List<Department> findAllByOrderByIdAsc();
 }
